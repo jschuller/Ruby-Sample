ce91deefa0374486ae72ce6f6721652d3b0933c1
Joshua Schuller <joshua_schuller@yahoo.com>
2011-07-03 17:51:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DumpTruck::Application.config.secret_token = '9367f707cd398249e518848cddd1f4e98aaf964f1698d08f18a8ec18cdea1203792d3a19ac08dd671f6b1265f89c0a7a6f1e0d19e9a77925bcff08f22d12fcd3'
